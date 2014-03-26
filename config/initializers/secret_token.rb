e4751afcd9ed12fa8c61a7aa2aaab51c5e099ed9
David Edwards & Vanessa Sant'Anna <vanessa.santanna@gmail.com>
2014-03-25 20:27:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Retroactive::Application.config.secret_key_base = '91a478ac2c1ff862a893d144867976808760595deefcaa738c4056ea4f6fae3e1b98f72f7c113fc04e630bf87116836d9ad630d0358a2a9347f39d3c5dd88dbe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Retroactive::Application.config.session_store :cookie_store, key: '_retroactive_session'
